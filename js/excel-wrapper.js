7f3691afafdef614b79b11eefd81114eb8e40e8e
Murali <chkrish.9@dummy.com>
2017-06-15 16:39:14 +0530

    Solved IE issue

--- js/excel-wrapper.js
+++ js/excel-wrapper.js
@@ -1,18 +1,27 @@
+function fixdata(data) {
+	var o = "", l = 0, w = 10240;
+	for(; l<data.byteLength/w; ++l) o+=String.fromCharCode.apply(null,new Uint8Array(data.slice(l*w,l*w+w)));
+	o+=String.fromCharCode.apply(null, new Uint8Array(data.slice(l*w)));
+	return o;
+}
 function handleFile(e) {
     //Get the files from Upload control
+    var rABS = typeof FileReader !== "undefined" && typeof FileReader.prototype !== "undefined" && typeof FileReader.prototype.readAsBinaryString !== "undefined";
     var files = e.target.files;
     var i, f;
     //Loop through files
     for (i = 0, f = files[i]; i != files.length; ++i) {
         var reader = new FileReader();
         var name = f.name;
-        reader.readAsBinaryString(f);
+        if(rABS) reader.readAsBinaryString(f);
+	    else reader.readAsArrayBuffer(f);
         reader.onload = function (e) {
             $("#dvOutput").html("");
             var data = e.target.result;
             var result=[];
+            var arr = rABS ? data : btoa(fixdata(data));
             //reading data from excel
-            var workbook = XLSX.read(data, { type: 'binary' });
+            var workbook = XLSX.read(arr, { type: rABS ? 'binary' : 'base64' });
             //Getting the sheet names
             var sheet_name_list = workbook.SheetNames;
             var count=0;
